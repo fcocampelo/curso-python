41b9f3e66d8febca7293a0bf2862461e5392923a
Francsico Campelo <francsico.campelo@gmail.com>
2016-10-24 12:47:21 -0200

    Aula 6 finalizada

--- app.py
+++ app.py
@@ -15,11 +15,27 @@ def remover(nomes):
 	nome_remover = raw_input()
 	nomes.remove(nome_remover)
 
+def alterar(nomes):
+	print 'Qual nome vc gostaria de alterar?'
+	nome_aterar = raw_input()
+	if(nome_aterar in nomes):
+		print 'Informe o novo nome'
+		nomes[nomes.index(nome_aterar)] = raw_input()
+
+def procurar(nomes):
+	print 'Qual nome vc gostaria de procurar?'
+	nome_procurar = raw_input()
+	if(nome_procurar in nomes):
+		print 'Nome na lista'
+	else:
+		print 'Nome não está na lista'
+		
+
 def menu():
 	nomes = [] #nao podemos esquecer de inicializar a lista
 	escolha = ''
 	while(escolha != '0'):    
-		print 'Digite: 1 para cadastrar, 2 para listar, 3 para remover nome, 0 para terminar'
+		print 'Digite: 1 para cadastrar, 2 para listar, 3 para remover nome, 4 para alterar, 5 para procurar, 0 para terminar'
 		escolha = raw_input()
 		if(escolha == '1'):
 			cadastrar(nomes)
@@ -29,5 +45,11 @@ def menu():
 
 		if(escolha == '3'):
 			remover(nomes)
-			
+
+		if(escolha == '4'):
+			alterar(nomes)
+
+		if(escolha == '5'):
+			procurar(nomes)
+
 menu()
